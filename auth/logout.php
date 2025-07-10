107b4b5eb0aaac50b8e6fb32ee7ecd066930a581
Alvin Muriuki <ialvinmurithi@gmail.com>
2025-07-10 22:45:10 +0300

    comments

--- auth/logout.php
+++ auth/logout.php
@@ -2,7 +2,7 @@
 // It's good practice to start the session to ensure we can destroy it.
 session_start();
 
-// Unset all of the session variables.
+// This line immediately clears all the data associated with the current session. For example, user_id, user_fname, and role_name are all removed.         
 $_SESSION = array();
 
 // If it's desired to kill the session, also delete the session cookie.
